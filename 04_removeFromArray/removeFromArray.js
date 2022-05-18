c7f579379571459f2da2879632625b65a55b96d8
Itallo <italloalexsander@gmail.com>
2022-05-18 14:27:05 -0300

    Finish first solution of 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, element) {
+    for(i = 0; i < array.length; i++){
+        if(array[i] === element){
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
