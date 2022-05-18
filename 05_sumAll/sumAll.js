d44c4dda9834b39abda5641c182464604b9861a8
Itallo <italloalexsander@gmail.com>
2022-05-18 14:45:55 -0300

    Solve exercise 5 sumAll using typeof

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(floor, ceiling) {
+    sum = 0;
+    if(floor > ceiling){
+        aux = ceiling;
+        ceiling = floor;
+        floor = aux;
+    }
+    if((floor < 0) || (ceiling < 0)){
+        return "ERROR"
+    }
+    if((typeof floor != "number") || (typeof ceiling != "number")){
+        return "ERROR"
+    }
+    for(i = floor; i <= ceiling; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
