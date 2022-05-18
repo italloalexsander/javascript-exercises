b80e61c3065e676ab0565eeeeffa115394b390da
Itallo <italloalexsander@gmail.com>
2022-05-18 14:13:50 -0300

    Solve exercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    tamanhoString = string.length;
+    j = tamanhoString - 1;
+    stringReversa = []
+    while(j >= 0){
+        stringReversa.push(string[j])
+        j--;
+    }
+    return stringReversa.join("");
 };
 
 // Do not edit below this line
