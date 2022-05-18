5eb5c05ae0ccbfca54bad55f6f85c1b68b8aa41b
Itallo <italloalexsander@gmail.com>
2022-05-18 15:08:29 -0300

    Solve exercise 7 using Math.round and parseFloat

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  convertedTemp = (temp - 32) * (5/9);
+  if(Math.round(convertedTemp) === convertedTemp){
+    return convertedTemp;
+  }else{
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  convertedTemp = temp * (9/5) + 32;
+  if(Math.round(convertedTemp) === convertedTemp){
+    return convertedTemp;
+  }else{
+    return parseFloat(convertedTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
