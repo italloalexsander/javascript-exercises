38a222af9d7c4fa61d03e7bcd21a3b3477bae750
Itallo <italloalexsander@gmail.com>
2022-05-18 14:35:03 -0300

    Solve exercise 4 removeFromArray completely

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
-const removeFromArray = function(array, element) {
-    for(i = 0; i < array.length; i++){
-        if(array[i] === element){
-            array.splice(i, 1);
+const removeFromArray = function(array) {
+    for(i = 1; i < arguments.length; i++){
+        for(j = 0; j < array.length; j++){
+            if(array[j] === arguments[i]){
+                array.splice(j,1);
+            }
         }
     }
     return array;
