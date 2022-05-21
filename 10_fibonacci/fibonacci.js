d8a8f144313d2547a0790e6fb97ec3ff1e1058b5
Itallo <italloalexsander@gmail.com>
2022-05-21 19:03:00 -0300

    Solve fibonacci exercise with validation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index <= 0){
+        return "OOPS";
+    }
+    if(typeof index !== "number"){
+        index = parseInt(index);
+    }
+    const fibbo = [];
+    let fibboValue = 1;
+    let prevValue = 0;
+    let aux = 0;
+    for(i = 1; i <= index; i++){
+        fibbo.push(fibboValue)
+        aux = fibboValue;
+        fibboValue += prevValue;
+        prevValue = aux;
+    }
+    console.log(fibbo);
+    return fibbo[index-1];
 };
 
 // Do not edit below this line
