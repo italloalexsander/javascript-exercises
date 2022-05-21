bb98e10558024ef7ff16ecd1d6516b71b76296d4
Itallo <italloalexsander@gmail.com>
2022-05-21 18:48:11 -0300

    Solve palindrome exercise with validation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.split(',').join('');
+    string = string.split('.').join('');
+    string = string.split(' ').join('');
+    string = string.split('!').join('');
+    auxString = string;
+    tamString = string.length;
 
+    
+    for(i = 0; i < tamString; i++){
+        if(string[i].toLowerCase() !== auxString[(tamString - i) - 1].toLowerCase()){
+            console.log(auxString + "\n" + string);
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
