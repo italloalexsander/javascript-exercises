b3cce1488b40ccde97c244c27e1b18d2a9bd590d
Itallo <italloalexsander@gmail.com>
2022-05-21 18:26:33 -0300

    Solve exercise 8: calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  }, 0)
 };
 
 const multiply = function() {
-
+  return Array.from(arguments[0]).reduce((previousValue, currentValue) => {
+    return previousValue * currentValue;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+  if(number === 0){
+    return 1;
+  }
+  else{
+    let factResult = 1;
+    for(i = 1; i <= number; i++){
+      factResult *= i;
+    }
+    return factResult;
+  }
+}
 
 // Do not edit below this line
 module.exports = {
