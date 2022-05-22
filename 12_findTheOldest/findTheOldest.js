e8f649c01fdb481e979e55961a13aaa822d26cab
Itallo <italloalexsander@gmail.com>
2022-05-21 21:37:39 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let person = {};
+    people.forEach(element => {
+        if(!element.yearOfDeath){
+            currentYear = new Date().getFullYear();
+            if(currentYear - element.yearOfBirth > maxAge){
+                maxAge = currentYear - element.yearOfBirth;
+                person = element;
+            }
+        }else{
+            if(element.yearOfDeath - element.yearOfBirth > maxAge){
+                maxAge = element.yearOfDeath - element.yearOfBirth;
+                person = element;
+            }
+        }
+    })
+    return person;
 };
 
 // Do not edit below this line
